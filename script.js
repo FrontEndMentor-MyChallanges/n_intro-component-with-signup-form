7859779235774245ae560fa884452c09bcf693cf
miroslaw-mroczkowski <mroczkowski.miroslaw@gmail.com>
2023-10-10 13:57:58 +0200

    html, tailwind classes refactored, started js

--- script.js
+++ script.js
@@ -10,11 +10,29 @@ const nameRegex = new RegExp(/^[a-zA-Z ]{2,30}$/);
 const emailRegex = new RegExp(/^[A-Za-z0-9_!#$%&'*+\/=?`{|}~^.-]+@[A-Za-z0-9.-]+$/gm);
 const passwordRegex = new RegExp(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm);
 
-form.addEventListener('submit', function (e) {
-  e.preventDefault();
+// FIXME - create working validation and then refactor it
+// form.addEventListener('submit', function (e) {
+// 	e.preventDefault();
 
-  const checkFirstName = nameRegex.test(firstName);
-  const checkLastName = nameRegex.test(lastName);
-  const checkEmail = emailRegex.test(email);
-  const checkPassword = passwordRegex.test(password);
-});
+// 	let checkFirstName, checkLastName, checkEmail, checkPassword;
+
+// 	checkFirstName ??= nameRegex.test(firstName.value);
+// 	// checkLastName ??= nameRegex.test(lastName?.value);
+// 	// checkEmail ??= emailRegex.test(email.value);
+// 	// checkPassword ??= passwordRegex.test(password.value);
+
+// 	if (!checkFirstName) {
+// 		firstName.closest('div').querySelector('img').classList.remove('hidden');
+// 		firstName.closest('.input-container').querySelector('.errMsg').classList.remove('hidden');
+// 		firstName.closest('.input-container').querySelector('.errMsg').classList.add('flex');
+// 	} else {
+// 		firstName.closest('div').querySelector('img').classList.add('hidden');
+// 		firstName.closest('.input-container').querySelector('.errMsg').classList.add('hidden');
+// 		firstName.closest('.input-container').querySelector('.errMsg').classList.remove('flex');
+// 	}
+
+// 	// if (!checkFirstName) return;
+
+// 	// console.log(`${checkFirstName}`);
+// 	// console.log(`${checkLastName}`);
+// });
